47891cf7b97f7f689bdf5142b1e5a37a19620abd
Ice White <yashiro1899@gmail.com>
2016-12-03 15:49:16 +0800

    Updated dash.sh

--- dash.sh
+++ dash.sh
@@ -14,33 +14,26 @@ mv -f *.* Contents/Resources/Documents/
 
 cat > Contents/Info.plist << EOF
 <?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
-  <dict>
-    <key>CFBundleIdentifier</key>
-    <string>http</string>
-    <key>CFBundleName</key>
-    <string>HTTP</string>
-    <key>isJavaScriptEnabled</key>
-    <true/>
-    <key>DocSetPlatformFamily</key>
-    <string>http</string>
-    <key>dashIndexFilePath</key>
-    <string>developer.mozilla.org/en-US/docs/Web/HTTP.html</string>
-  </dict>
+<dict>
+  <key>CFBundleIdentifier</key>
+  <string>http</string>
+  <key>CFBundleName</key>
+  <string>HTTP</string>
+  <key>DocSetPlatformFamily</key>
+  <string>http</string>
+  <key>dashIndexFilePath</key>
+  <string>developer.mozilla.org/en-US/docs/Web/HTTP.html</string>
+  <key>isDashDocset</key>
+  <true/>
+  <key>isJavaScriptEnabled</key>
+  <true/>
+</dict>
 </plist>
 EOF
 
-cat > Contents/Resources/Nodes.xml << EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<DocSetNodes version="1.0">
-  <TOC>
-    <Node type="folder">
-      <Name>Documentation</Name>
-      <Path>index.html</Path>
-    </Node>
-  </TOC>
-</DocSetNodes>
-EOF
-
-#   mv HTML HTML.docset && \
-#   /Developer/usr/bin/docsetutil index HTML.docset && \
+cd - && \
+cp icon.png HTTP && \
+mv HTTP HTTP.docset
+node .
